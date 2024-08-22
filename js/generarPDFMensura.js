36112c1fada096fdb36328aec9a7153e450e15cd
FaustoPR <111300399+FaustoPR@users.noreply.github.com>
2024-08-22 17:31:07 -0300

    Add files via upload

--- js/generarPDFMensura.js
+++ js/generarPDFMensura.js
@@ -1,5 +1,6 @@
 
-export function generarPDF(solicitante, ubicacion, tipoObra, tipoConstruccion, superficieObra, referencia, importe20, importe21, importe25, importe26, importe29, importe31, importeTotal) {
+export function generarPDF(solicitante, nomenclatura, partida, objetoMensura, calculoA1, calculoA2, calculoB1, calculoB2, calculoB3, calculoB4, calculoB5, calculoC, calculoD1, calculoD2, calculoE, costoTotal)
+{
     const {jsPDF} = window.jspdf
     const pdf = new jsPDF("p", "pt", "a4")
     // fetch('montserrat-base64.txt').then(response => response.text()).then(montserratBase64 => {
@@ -26,7 +27,7 @@ export function generarPDF(solicitante, ubicacion, tipoObra, tipoConstruccion, s
     pdf.setFontSize(11).split
     pdf.setTextColor(0,0,0)
     pdf.text('Según ordenanza tarifaria 2024',300,45)
-    pdf.text('"Capitulo 10: Derechos de construcción"',300,60)
+    pdf.text('"Capitulo 10: Derechos de catastro"',300,60)
 
     //aca va fecha, solicitante, etc
     pdf.setFillColor(0,127,198)
@@ -35,29 +36,20 @@ export function generarPDF(solicitante, ubicacion, tipoObra, tipoConstruccion, s
     pdf.rect(20,105,255,20,"F")
     pdf.rect(20,145,255,20,"F")
     pdf.rect(20,185,255,20,"F")
-    pdf.rect(20,225,255,20,"F")
     pdf.rect(320,105,255,20,"F")
     pdf.rect(320,145,255,20,"F")
-    pdf.rect(320,185,255,35,"F")
+    // pdf.rect(320,145,255,20,"F")
+    // pdf.rect(320,185,255,35,"F")
     pdf.text("Fecha: ",20,100)
     pdf.text(`${sacarFecha()}`,25,120)
     pdf.text("Solicitante: ",20,140)
     pdf.text(`${solicitante}`,25,160)
-    pdf.text("Ubicacion de la obra: ",20,180)
-    pdf.text(`${ubicacion}`,25,200)
-    pdf.text("Referencia: ",20,220)
-    pdf.text(`${referencia}`,25,240)
-    pdf.text("Tipo de obra: ",320,100)
-    pdf.text(`${tipoObra}`,325,120)
-    pdf.text("Superficie de obra en m2: ",320,140)
-    pdf.text(`${superficieObra}`,325,160)
-    pdf.text("Tipo de construcción/es: ",320,180)
-    const cantLineasTipoConstruccion = pdf.splitTextToSize(`${tipoConstruccion}`,245)
-    let alturaTipoConstruccion = 200
-    cantLineasTipoConstruccion.forEach(linea => {
-        pdf.text(linea,325,alturaTipoConstruccion)
-        alturaTipoConstruccion += 15
-    })
+    pdf.text("Nomenclatura Catastral: ",20,180)
+    pdf.text(`${nomenclatura}`,25,200)
+    pdf.text("Partida: ",320,100)
+    pdf.text(`${partida}`,325,120)
+    pdf.text("Objeto de Mensura: ",320,140)
+    pdf.text(`${objetoMensura}`,325,160)
 
 
     //Linea azul
@@ -65,17 +57,22 @@ export function generarPDF(solicitante, ubicacion, tipoObra, tipoConstruccion, s
     pdf.rect(0,260,595,15,"F")
 
     //Formatear importes
-    const importe20Formateado = formatearNumero(importe20)
-    const importe21Formateado = formatearNumero(importe21)
-    const importe25Formateado = formatearNumero(importe25)
-    const importe26Formateado = formatearNumero(importe26)
-    const importe29Formateado = formatearNumero(importe29)
-    const importe31Formateado = formatearNumero(importe31)
-    const importeTotalFormateado = formatearNumero(importeTotal)
+    const calculoA1Formateado = formatearNumero(calculoA1)
+    const calculoA2Formateado = formatearNumero(calculoA2)
+    const calculoB1Formateado = formatearNumero(calculoB1)
+    const calculoB2Formateado = formatearNumero(calculoB2)
+    const calculoB3Formateado = formatearNumero(calculoB3)
+    const calculoB4Formateado = formatearNumero(calculoB4)
+    const calculoB5Formateado = formatearNumero(calculoB5)
+    const calculoCFormateado = formatearNumero(calculoC)
+    const calculoD1Formateado = formatearNumero(calculoD1)
+    const calculoD2Formateado = formatearNumero(calculoD2)
+    const calculoEFormateado = formatearNumero(calculoE)
+    const importeTotalFormateado = formatearNumero(costoTotal)
 
 
     //Articulos
-    pdf.text("Corresponde a cada articulo la siguiente suma: ",20,300)
+    pdf.text("Corresponde a cada articulo el siguiente importe: ",20,300)
 
     pdf.setFillColor (185,185,185)
     pdf.rect(20,320,115,20,"F")
@@ -86,7 +83,12 @@ export function generarPDF(solicitante, ubicacion, tipoObra, tipoConstruccion, s
     pdf.rect(135,420,140,20,"F")
     pdf.rect(20,440,115,20,"F")
     pdf.rect(135,460,140,20,"F")
-    // pdf.rect(135,500,140,20,"F")
+    pdf.rect(20,480,115,20,"F")
+    pdf.rect(135,500,140,20,"F")
+    pdf.rect(20,520,115,20,"F")
+    pdf.rect(135,540,140,20,"F")
+    // pdf.rect(20,560,115,20,"F")
+    pdf.rect(135,580,140,20,"F")
 
     pdf.setFillColor (197,197,197) 
     pdf.rect(135,320,140,20,"F")
@@ -96,27 +98,39 @@ export function generarPDF(solicitante, ubicacion, tipoObra, tipoConstruccion, s
     pdf.rect(135,400,140,20,"F")
     pdf.rect(20,420,115,20,"F")
     pdf.rect(135,440,140,20,"F")
-    // pdf.rect(20,460,115,20,"F")
+    pdf.rect(20,460,115,20,"F")
     pdf.rect(135,480,140,20,"F")
+    pdf.rect(20,500,115,20,"F")
+    pdf.rect(135,520,140,20,"F")
+    pdf.rect(20,540,115,20,"F")
+    pdf.rect(135,560,140,20,"F")
 
     pdf.text("Articulo",25,335)
     pdf.text("Importe",140,335)
-    pdf.text("20°",25,355)
-    pdf.text(`$${importe20Formateado}`,140,355)
-    pdf.text("21°",25,375)
-    pdf.text(`$${importe21Formateado}`,140,375)
-    pdf.text("25°",25,395)
-    pdf.text(`$${importe25Formateado}`,140,395)
-    pdf.text("26°",25,415)
-    pdf.text(`$${importe26Formateado}`,140,415)
-    pdf.text("29°",25,435)
-    pdf.text(`$${importe29Formateado}`,140,435)
-    // pdf.text("30°",25,455)
-    // pdf.text(`${importe30}`,140,455)
-    pdf.text("31°",25,455)
-    pdf.text(`$${importe31Formateado}`,140,455)
-    pdf.text("Total",140,475)
-    pdf.text(`$${importeTotalFormateado}`,140,495)
+    pdf.text("19° - a1",25,355)
+    pdf.text(`$${calculoA1Formateado}`,140,355)
+    pdf.text("19° - a2",25,375)
+    pdf.text(`$${calculoA2Formateado}`,140,375)
+    pdf.text("19° - b1",25,395)
+    pdf.text(`$${calculoB1Formateado}`,140,395)
+    pdf.text("19° - b2",25,415)
+    pdf.text(`$${calculoB2Formateado}`,140,415)
+    pdf.text("19° - b3",25,435)
+    pdf.text(`$${calculoB3Formateado}`,140,435)
+    pdf.text("19° - b4",25,455)
+    pdf.text(`$${calculoB4Formateado}`,140,455)
+    pdf.text("19° - b5",25,475)
+    pdf.text(`$${calculoB5Formateado}`,140,475)
+    pdf.text("19° - c1",25,495)
+    pdf.text(`$${calculoCFormateado}`,140,495)
+    pdf.text("19° - d1",25,515)
+    pdf.text(`$${calculoD1Formateado}`,140,515)
+    pdf.text("19° - d2",25,535)
+    pdf.text(`$${calculoD2Formateado}`,140,535)
+    pdf.text("19° - e1",25,555)
+    pdf.text(`$${calculoEFormateado}`,140,555)
+    pdf.text("Total",140,575)
+    pdf.text(`$${importeTotalFormateado}`,140,595)
 
     const urlImagen = "imgs/logo_anguil_40.jpg"
     
---
+++ js/mensura.js
@@ -0,0 +1,419 @@
+import {generarPDF} from './generarPDFMensura.js'
+
+document.addEventListener('DOMContentLoaded', function(){
+    /////////////////////////////////////////////////////////////////////
+    // Reiniciar checkbox/input/radio al reiniciar pagina //
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    const radios = document.querySelectorAll('input[type="radio"]');
+    const inputs = document.querySelectorAll('input[type="text"]');
+    const inputsMenosPopup = document.querySelectorAll('input[type="text"]:not(.inputPopup)');
+    // checkboxes.forEach(checkbox => {
+    //     checkbox.addEventListener('change', validateAndToggleButton);
+    // })
+    function addCheckboxListeners() {
+        const A1Checkbox = document.getElementById('a1');
+        const A2Checkbox = document.getElementById('a2');
+        const B1Checkbox = document.getElementById('b1');
+        const B5Checkbox = document.getElementById('b5');
+        const E1Checkbox = document.getElementById('e1');
+    
+        A1Checkbox.addEventListener('change', validateAndToggleButton);
+        A2Checkbox.addEventListener('change', validateAndToggleButton);
+        B1Checkbox.addEventListener('change', validateAndToggleButton);
+        B5Checkbox.addEventListener('change', validateAndToggleButton);
+        E1Checkbox.addEventListener('change', validateAndToggleButton);
+    }
+    addCheckboxListeners()
+
+    function resetCheckboxRadioInput() {
+        checkboxes.forEach(checkbox => {
+            checkbox.checked = false;
+        });
+        radios.forEach(radio => {
+            radio.checked = false;
+        });
+        inputs.forEach(input => {
+            input.value = '';
+        });
+        botonCalculo.disabled = true
+        botonCalcularEImprimir.disabled = true
+    }
+    ////////////////////////////////////////////////////////////////////
+    // Calculo de resultado //
+    function calculoA1() {
+        const A1Checkbox = document.getElementById('a1')
+        let costoA1 = 0
+        if (A1Checkbox.checked) {
+            costoA1 += 1500
+        }
+        return costoA1
+    }
+
+    function calculoA2() {
+        const A2Checkbox = document.getElementById('a2')
+        let costoA2 = 0
+        if (A2Checkbox.checked) {
+            costoA2 += 3000
+        }
+        return costoA2
+    }
+
+    function calculoA() {
+        let costoA = 0
+        costoA += calculoA1()
+        costoA += calculoA2()
+        return costoA
+    }
+
+    function calculoB1() {
+        const B1Checkbox = document.getElementById('b1')
+        let costoB1 = 0
+        if (B1Checkbox.checked) {
+            costoB1 += 5940
+        }
+        return costoB1
+    }
+
+    function calculoB2() {
+        const B2Checkbox = document.getElementById('b2')
+        const inputCantidadB2 = document.getElementById('cantidadB2')
+        let costoB2 = 0
+        if (B2Checkbox.checked) {
+            costoB2 += parseInt(inputCantidadB2.value) * 2430 
+        }
+        return costoB2 
+    }
+
+    function calculoB3() {
+        const B3Checkbox = document.getElementById('b3')
+        const inputCantidadB3 = document.getElementById('cantidadB3')
+        let costoB3 = 0
+        if (B3Checkbox.checked) {
+            costoB3 += parseInt(inputCantidadB3.value) * 2430
+        }
+        return costoB3
+    }
+
+    function calculoB4() {
+        const B4Checkbox = document.getElementById('b4')
+        const inputCantidadB4 = document.getElementById('cantidadB4')
+        let costoB4 = 0
+        if (B4Checkbox.checked) {
+            costoB4 += parseInt(inputCantidadB4.value) * 2430
+        }
+        return costoB4
+    }
+
+    function calculoB5() {
+        const B5Checkbox = document.getElementById('b5')
+        let costoB5 = 0
+        if (B5Checkbox.checked) {
+            costoB5 += 12000
+        }
+        return costoB5
+    }
+
+    function calculoB() {
+        let costoB = 0
+        costoB += calculoB1() 
+        costoB += calculoB2()
+        costoB += calculoB3()
+        costoB += calculoB4()
+        costoB += calculoB5()
+        return costoB
+    }
+
+    function calculoC() {
+        const C1Checkbox = document.getElementById('c1')
+        const inputCantidadC1 = document.getElementById('cantidadC1')
+        let costoC = 0
+        if (C1Checkbox.checked) {
+            let costoC1 = parseInt(inputCantidadC1.value) * 2970
+            costoC += costoC1
+        }
+        return costoC
+    }
+
+    function calculoD1() {
+        const D1Checkbox = document.getElementById('d1')
+        const inputCantidadD1 = document.getElementById('cantidadD1')
+        let costoD1 = 0
+        if (D1Checkbox.checked) {
+            costoD1 += parseInt(inputCantidadD1.value) * 4050
+        }
+        return costoD1
+    }
+
+    function calculoD2() {
+        const D2Checkbox = document.getElementById('d2')
+        const inputCantidadD2 = document.getElementById('cantidadD2')
+        let costoD2 = 0
+        if (D2Checkbox.checked) {
+            costoD2 += parseInt(inputCantidadD2.value) * 4050
+        }
+        return costoD2
+    }
+
+    function calculoD() {
+        let costoD = 0
+        costoD += calculoD1()
+        costoD += calculoD2()
+        return costoD
+    }
+
+    function calculoE() {
+        const E1Checkbox = document.getElementById('e1')
+        const inputCantidadE1 = document.getElementById('cantidadE1')
+        let costoE = 0
+        if (E1Checkbox.checked) {
+            costoE += 16000
+        }
+        return costoE
+    }
+
+    function calculoTotal() {
+        let costoTotal = 0
+        costoTotal += calculoA()
+        costoTotal += calculoB()
+        costoTotal += calculoC()
+        costoTotal += calculoD()
+        costoTotal += calculoE()
+        return costoTotal
+    }
+
+
+    // ***** Funciones para imprimir ***** //
+
+    function formatearNumero(num) {
+        // Convert the number to a string and split it by the decimal point
+        let [integerPart, decimalPart] = num.toString().split('.');
+      
+        // Add a period every three digits in the integer part
+        integerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+
+        // Limit the decimal part to a maximum of three digits
+        if (decimalPart) {
+            decimalPart = decimalPart.slice(0, 3);  
+        }
+    
+        // Combine the integer part and decimal part, replacing the original decimal point with a comma
+        return decimalPart ? `${integerPart},${decimalPart}` : integerPart;
+    }
+
+    // ***** Botones Calculo y Imprimir ***** //
+    const botonCalculo = document.getElementById('botonCalculo') 
+    const botonCalcularEImprimir = document.getElementById('botonCalcularEImprimir')
+    const botonImprimir = document.getElementById('botonImprimir')
+    const botonCerrarPopup = document.getElementById('botonCerrarPopup')
+    const botonLimpiar = document.getElementById('botonLimpiar')
+
+    botonCerrarPopup.addEventListener('click', function() {
+        const popup = document.getElementById("popup")
+        popup.style.display = "none"
+    })
+    
+    botonCalculo.addEventListener('click', function(e) {
+        e.preventDefault();
+        let costoTotal = calculoTotal()
+        const resultado = document.getElementById('resultado');
+        let costoTotalFormateado = formatearNumero(costoTotal)
+        resultado.textContent = "$" + costoTotalFormateado;
+
+    })
+
+    botonCalcularEImprimir.addEventListener('click', function(e) {
+        e.preventDefault();
+        const popup = document.getElementById("popup")
+        popup.style.display = "flex"
+        let costoTotal = calculoTotal()
+        const resultado = document.getElementById('resultado');
+        let costoTotalFormateado = formatearNumero(costoTotal)
+        resultado.textContent = "$" + costoTotalFormateado;
+    })
+
+    botonImprimir.addEventListener('click', function(e) {
+        e.preventDefault()
+        const solicitante = document.getElementById('popupNombre').value
+        const nomenclatura = document.getElementById('popupNomenclatura').value
+        const partida = document.getElementById('popupPartida').value
+        const objetoMensura = document.getElementById('popupObjetoMensura').value
+        let costoTotal = calculoTotal()
+        generarPDF(solicitante, nomenclatura, partida, objetoMensura, calculoA1(), calculoA2(), calculoB1(), calculoB2(), calculoB3(), calculoB4(), calculoB5(), calculoC(), calculoD1(), calculoD2(), calculoE(), costoTotal)
+    })
+
+    botonLimpiar.addEventListener('click', function(e) {
+        e.preventDefault()
+        checkboxes.forEach(checkbox => {
+            checkbox.checked = false;
+        });
+        radios.forEach(radio => {
+            radio.checked = false;
+        });
+        inputs.forEach(input => {
+            input.value = '';
+        });
+        botonCalculo.disabled = true
+        botonCalcularEImprimir.disabled = true
+
+        // DESACTIVAR TODOS LOS DIV
+        const divB2 = document.getElementById('divB2')
+        const divB3 = document.getElementById('divB3')
+        const divB4 = document.getElementById('divB4')
+        const divC1 = document.getElementById('divC1')
+        const divD1 = document.getElementById('divD1')
+        const divD2 = document.getElementById('divD2')
+        divB2.style.display = 'none'
+        divB3.style.display = 'none'
+        divB4.style.display = 'none'
+        divC1.style.display = 'none'
+        divD1.style.display = 'none'
+        divD2.style.display = 'none'
+    })
+
+    function validateAndToggleButton() {
+        let allFilled = true;
+        let checkboxChecked = false;
+        let anyVisibleInputs = false; // Flag to track if there are any visible inputs
+        inputsMenosPopup.forEach(input => {
+            if (isElementVisible(input)) {
+                anyVisibleInputs = true; // At least one input is visible
+                input.value = input.value.replace(/[^\d]/g, '');
+                if (input.value === '') {
+                    allFilled = false;
+                }
+            }
+        });
+    
+        // If no inputs are visible, set allFilled to false
+        if (!anyVisibleInputs) {
+            allFilled = false;
+        }
+
+        const A1Checkbox = document.getElementById('a1')
+        const A2Checkbox = document.getElementById('a2')
+        const B1Checkbox = document.getElementById('b1')
+        const B5Checkbox = document.getElementById('b5')
+        const E1Checkbox = document.getElementById('e1')
+        console.log(`A1 checked: ${A1Checkbox.checked}`);
+        console.log(`A2 checked: ${A2Checkbox.checked}`);
+        console.log(`B1 checked: ${B1Checkbox.checked}`);
+        console.log(`B5 checked: ${B5Checkbox.checked}`);
+        console.log(`E1 checked: ${E1Checkbox.checked}`);
+
+        if (A1Checkbox.checked || A2Checkbox.checked || B1Checkbox.checked || B5Checkbox.checked || E1Checkbox.checked) {
+            checkboxChecked = true;
+        }
+    
+        // Enable buttons if all inputs are filled or if checkbox A or C is checked
+        // const enableButtons = allFilled && checkboxChecked;
+        const enableButtons = allFilled || checkboxChecked;
+        console.log(`All inputs filled: ${allFilled}`);
+        console.log(`Checkbox checked: ${checkboxChecked}`);
+        console.log(`Enable buttons: ${enableButtons}`);
+        botonCalculo.disabled = !enableButtons;
+        botonCalcularEImprimir.disabled = !enableButtons;
+        // botonCalculo.disabled = !allFilled;
+        // botonCalcularEImprimir.disabled = !allFilled;
+    }
+    
+    inputsMenosPopup.forEach(input => {
+        input.addEventListener('input', validateAndToggleButton);
+    });
+    
+    function isElementVisible(element) {
+        return element.offsetParent !== null;
+    }
+
+
+    /////////////////////////////////////////////////////////////
+    // Aparicion de div //
+
+    const B2Checkbox = document.getElementById('b2')
+    const B3Checkbox = document.getElementById('b3')
+    const B4Checkbox = document.getElementById('b4')
+    const C1Checkbox = document.getElementById('c1')
+    const D1Checkbox = document.getElementById('d1')
+    const D2Checkbox = document.getElementById('d2')
+
+    const divB2 = document.getElementById('divB2')
+    const divB3 = document.getElementById('divB3')
+    const divB4 = document.getElementById('divB4')
+    const divC1 = document.getElementById('divC1')
+    const divD1 = document.getElementById('divD1')
+    const divD2 = document.getElementById('divD2')
+
+   
+
+    B2Checkbox.addEventListener('change', function() {
+        if (B2Checkbox.checked) {
+            divB2.style.display = 'flex'
+        }
+        else {
+            divB2.style.display = 'none'
+            const inputCantidadB2 = document.getElementById('cantidadB2')
+            inputCantidadB2.value = ''
+            validateAndToggleButton()
+        }
+    })
+
+    B3Checkbox.addEventListener('change', function() {
+        if (B3Checkbox.checked) {
+            divB3.style.display = 'flex'
+        }
+        else {
+            divB3.style.display = 'none'
+            const inputCantidadB3 = document.getElementById('cantidadB3')
+            inputCantidadB3.value = ''
+            validateAndToggleButton()
+        }
+    })
+
+    B4Checkbox.addEventListener('change', function() {
+        if (B4Checkbox.checked) {
+            divB4.style.display = 'flex'
+        }
+        else {
+            divB4.style.display = 'none'
+            const inputCantidadB4 = document.getElementById('cantidadB4')
+            inputCantidadB4.value = ''
+            validateAndToggleButton()
+        }
+    })
+
+    C1Checkbox.addEventListener('change', function() {
+        if (C1Checkbox.checked) {
+            divC1.style.display = 'flex'
+        }
+        else {
+            divC1.style.display = 'none'
+            const inputCantidadC1 = document.getElementById('cantidadC1')
+            inputCantidadC1.value = ''
+            validateAndToggleButton()
+        }
+    })
+
+    D1Checkbox.addEventListener('change', function() {
+        if (D1Checkbox.checked) {
+            divD1.style.display = 'flex'
+        }
+        else {
+            divD1.style.display = 'none'
+            const inputCantidadD1 = document.getElementById('cantidadD1')
+            inputCantidadD1.value = ''
+            validateAndToggleButton()
+        }
+    })
+
+    D2Checkbox.addEventListener('change', function() {
+        if (D2Checkbox.checked) {
+            divD2.style.display = 'flex'
+        }
+        else {
+            divD2.style.display = 'none'
+            const inputCantidadD2 = document.getElementById('cantidadD2')
+            inputCantidadD2.value = ''
+            validateAndToggleButton()
+        }
+    })
+    resetCheckboxRadioInput();
+})
